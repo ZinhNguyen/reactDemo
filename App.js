bf86f647c5d6b7d740e294fa3cc237345259e463
ZinhNguyen <67100275+ZinhNguyen@users.noreply.github.com>
2021-11-29 21:14:14 +0700

    29/11

--- App.js
+++ App.js
@@ -9,10 +9,14 @@
 import React, {Component} from 'react';
 import {Text, View, StyleSheet, TextInput, Button, Animated} from 'react-native';
 import Home from "./home";
-import Detail from './details';
+//import Detail from './details';
 import 'react-native-gesture-handler';
 import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
+import IconAg from 'react-native-vector-icons/MaterialIcons';
+//import IconAg from 'react-native-vector-icons/AntDesign';
+import { createDrawerNavigator } from '@react-navigation/drawer';
 import Mh_flatlist from './mh_flatlist';
 import Mh_sectlist from './mh_sectlist';
 import FinalMain from './final_main';
@@ -20,26 +24,95 @@ import FinalLogin from './final_login';
 import FinalSignin from './final_signin';
 import Mh_LoaiHoa from './mh_loaihoa';
 import Mh_Hoa from './mh_hoa';
+import Mh_Selectlist from './Mh_Selectlist';
 
 
+const MaterdetailNav=createStackNavigator();
+const Tab= createBottomTabNavigator();
+const Drawer = createDrawerNavigator();
 
-const Stack=createStackNavigator();
+const Materdetail=()=>(
+  <MaterdetailNav.Navigator
+  initialRouteName='Mater'
+  options={{headerTitleAlign:'center'}}
+  >
+    <MaterdetailNav.Screen
+    name='Mater'
+    component={Mh_LoaiHoa}
+    options={{title:'Danh Sách Loài Hoa',
+    headerTitleAlign: 'center'
+    }}
+    />
+    <MaterdetailNav.Screen
+    name='Detail'
+    component={Mh_Hoa}
+    options={({route}) => ({
+      title: route.params.tenloai,
+      headerTitleAlign:'center',
+    })}
+    />
+  </MaterdetailNav.Navigator>
+)
+
+const TabControl=() => (
+  <Tab.Navigator>
+    <Tab.Screen name= "Master Detail" 
+    component={Materdetail}
+    options={{
+      tabBarLabel: 'Home',
+      tabBarIcon: ({ color, size}) => (
+        <IconAg name="favorite" color={color} size={size} />)
+    }}/>
+     <Tab.Screen name= "Setting" 
+    component={Mh_sectlist}
+    options={{
+      tabBarLabel: 'Select',
+      tabBarIcon: ({ color, size}) => (
+        <IconAg name="settings" color={color} size={size} />)
+    }}/> 
+   
+  </Tab.Navigator>
+)
 
 class MyApp extends Component {
   render() {
     return (
       <>
       <NavigationContainer>
-        <Stack.Navigator
-          initialRouteName='Loaihoa'
+        <Drawer.Navigator>
+          <Drawer.Screen name = "Home" 
+          component={TabControl}
+          options={{
+            drawerLabel: 'Home',
+            drawerIcon: ({color, size}) => (
+              <IconAg name="home" color={color} size={size} />)
+          }}/>
+          <Drawer.Screen name="Flat" 
+          component={Mh_flatlist}
+          options={{
+            drawerLabel: 'Select',
+            drawerIcon: ({color, size}) => (
+              <IconAg name="camera" color={color} size={size} />)
+          }}/>
+          <Drawer.Screen name="Final" 
+          component={FinalMain}
+          options={{
+            drawerLabel: 'Final',
+            drawerIcon: ({color, size}) => (
+              <IconAg name="camera" color={color} size={size} />)
+          }}/>
+        </Drawer.Navigator>
+        {/* <Stack.Navigator
+          initialRouteName='Master'
             options={{
               headerTitleAlign: 'center',
             }}
         >
           <Stack.Screen
-            name='Loaihoa'
+            name='Master'
             component={Mh_LoaiHoa}
             options={{
+              title: 'Danh Sách Loại Hoa',
               headerTitleAlign:'center',
               
             }}
@@ -55,10 +128,11 @@ class MyApp extends Component {
           <Stack.Screen
             name='Detail'
             component={Mh_Hoa}
-            options={{
+            options={({route}) => ({
+              title: route.params.tenloai,
               headerTitleAlign:'center',
               
-            }}
+            })}
             />
           <Stack.Screen
             name='MhFlat'
@@ -98,7 +172,7 @@ class MyApp extends Component {
               headerTitleAlign:'center',
             }}
             />
-            </Stack.Navigator>           
+            </Stack.Navigator>            */}
       </NavigationContainer>
       </>
     )
